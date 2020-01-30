253f475b05295447622eb85919e68a7da548353a
Артем Данильченко <art_injener@mail.ru>
2020-01-30 15:42:28 +0300

    Правка readMe

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,5 @@ insert into event(id, user_id, title, start, end, description) values (7, 1, 'ev
 insert into event(id, user_id, title, start, end, description) values (8, 1, 'event8', '2020-01-27 8:00:00', '2020-01-27 8:30:00', 'description1')
 insert into event(id, user_id, title, start, end, description) values (9, 1, 'event9', '2020-01-27 9:00:00', '2020-01-27 9:30:00', 'description1')
 
-insert into usr(id, username, password, active) values (1, 'admin', 'admin', 1);
+insert into usr(id, username, password, active) values (1, 'test', 'test', 1);
 insert into usr(id, username, password, active) values (2, 'user', 'user', 1);
